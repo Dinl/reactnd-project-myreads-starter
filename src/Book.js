cd884eab9fbd3624d5e7c559fb73ee420d7ec7eb
Daniel Gomez <dinl_@hotmail.com>
2017-10-16 20:27:19 -0500

    Bux fix in the submenu, now is tied to the instance

--- src/Book.js
+++ src/Book.js
@@ -25,12 +25,12 @@ class Book extends Component {
                     }
                 </div>
                 <div className="book-shelf-changer">
-                    <select onChange={this.changeReadStatus}>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
+                    <select onChange={this.changeReadStatus} value={details.shelf}>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
                     </select>
                 </div>
                 </div>
